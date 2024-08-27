1b16473664293b985d2aae247b6b13d471eea8d4
JustUzair <uzairhajra763390@gmail.com>
2024-08-27 18:07:29 +0530

    Added more tests

--- test/ethereum/MinimalAccountTest.t.sol
+++ test/ethereum/MinimalAccountTest.t.sol
@@ -7,6 +7,7 @@ import {DeployMinimal} from "script/DeployMinimal.s.sol";
 import {SendPackedUserOp} from "script/SendPackedUserOp.s.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
 import {ERC20Mock as ERC20} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 // https://youtu.be/mmzkPz71QJs?t=5690
 
@@ -53,13 +54,35 @@ contract MinimalAccountTest is Test {
         vm.deal(minimalAccount.owner(), 100 ether);
         assertEq(minimalAccount.owner().balance, 100 ether);
         assertEq(address(receiver).balance, 0);
-
         address dest = payable(receiver);
         uint256 value = 2 ether;
-        bytes memory data = abi.encodeWithSignature("call()");
+        bytes memory data = "";
         vm.prank(minimalAccount.owner());
         minimalAccount.execute{value: value}(dest, value, data);
         assertEq(minimalAccount.owner().balance, 98 ether);
         assertEq(address(receiver).balance, 2 ether);
+    }
+
+    function test_ownerSendERC20ToEOA() public {
+        address receiver = makeAddr("receiver");
+        deal(address(usdc), address(minimalAccount), 100e18);
+        assertEq(usdc.balanceOf(address(minimalAccount)), 100e18);
+        assertEq(usdc.balanceOf(receiver), 0);
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory approveData = abi.encodeWithSignature("approve(address,uint256)", address(receiver), 2e18);
+        bytes memory transferData = abi.encodeWithSignature("transfer(address,uint256)", address(receiver), 2e18);
+
+        vm.startPrank(minimalAccount.owner());
+        // approve
+        minimalAccount.execute(dest, value, approveData);
+        // check if the allowance was updated
+        assertEq(usdc.allowance(address(minimalAccount), receiver), 2e18);
+        minimalAccount.execute(dest, value, transferData);
+
+        // transfer
+        vm.stopPrank();
+        assertEq(usdc.balanceOf(receiver), 2e18);
+        assertEq(usdc.balanceOf(address(minimalAccount)), 98e18);
     }
 }
